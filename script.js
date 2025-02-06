4516d94f09a2c292e595921280fa295beca3c0ff
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-06 12:59:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str.charAt(i)) === i && str.indexOf(str.charAt(i), i + 1) === -1) {
+      return str.charAt(i);
+    }
+  }
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
